85c4a998f6e5fc5ca61820489bd90fe276b60004
Mark Henderson <henderson.mark@gmail.com>
2020-05-05 23:16:45 -0400

    ok ok im really done this time

--- src/index.js
+++ src/index.js
@@ -5,40 +5,41 @@ const CryptoJS = require('crypto-js')
 const express = require('express')
 const TallyLabIdentities = require('tallylab-orbitdb-identity-provider')
 
-const start = async () => {
-  const app = express()
-  app.use(require('body-parser').json())
-  app.use(express.static('examples'))
-
+;(async () => {
   const { nacl, verifications, verificationChecks } = await require('./init')()
 
-  app.post('/verify', async function (req, res, next) {
+  async function verify (req, res, next) {
     try {
       const { to } = req.body
       const { sid, status } = await verifications.create({ to, channel: 'sms' })
       res.json({ sid, status })
     } catch (err) { next(err) }
-  })
+  }
 
-  app.post('/login', async function (req, res, next) {
+  async function checkVerification (req, res, next) {
     try {
       const { to, code } = req.body
       const { status } = await verificationChecks.create({ to, code })
       if (status === 'denied') return res.status(403).end('Verification Failed')
       next()
     } catch (err) { next(err) }
-  }, async function (req, res, next) {
-    const toNumber = req.body.phoneNumber
+  }
+
+  async function generateKeys (req, res, next) {
     try {
+      const toNumber = req.body.phoneNumber
       const idProvider = new TallyLabIdentities().TallyLabIdentityProvider
       const hash = CryptoJS.SHA256(toNumber + SALT)
       const buffer = Buffer.from(hash.toString(CryptoJS.enc.Hex), 'hex')
       const tlKeys = idProvider.keygen(nacl, buffer)
       res.json(tlKeys)
     } catch (e) { next(e) }
-  })
+  }
 
+  const app = express()
+  app.use(require('body-parser').json())
+  app.use(express.static('examples'))
+  app.post('/verify', verify)
+  app.post('/login', checkVerification, generateKeys)
   app.listen(HTTP_PORT, HTTP_HOST, () => {})
-}
-
-module.exports = { start }
+})()
